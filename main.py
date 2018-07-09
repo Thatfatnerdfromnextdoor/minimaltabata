b42f076eaabcc4505d5fa6322a605533f44dbea3
Thatfatnerdfromnextdoor <35805530+Thatfatnerdfromnextdoor@users.noreply.github.com>
2018-07-09 22:22:07 +0200

    Sound added

--- main.py
+++ main.py
@@ -13,13 +13,17 @@ print("starting tabata session with a duration of " + str(cycles * tabatas * (ex
 while ic<cycles:
     print("cycle number " + str(ic))
     it=0
+    os.system("nohup play resources/startbeep.wav</dev/null &>/dev/null")
+    time.sleep(3.5)
     while it<tabatas:
         print("tabata number " + str(it))
-        time.sleep(extime - 3)
-        os.system("play beep-01a.wav")
+        time.sleep(extime)
+        os.system("nohup play resources/pausebeep.wav</dev/null &>/dev/null")
         print("pause")
-        os.system("play beep-01a.wav")
-        time.sleep(ptime)
-        it+=1
+        it+=1 #tabata increment
+        time.sleep(ptime -3.5)
+        if it != tabatas:    #check if it is necessary to start sound(only false for last tabata of the current cycle)
+            os.system("nohup play resources/startbeep.wav</dev/null &>/dev/null")
+        time.sleep(3.5)
     ic+=1
 print("finished")
